a74e0f5692c00288bc073635d7d9c8292875462b
jetzable <gjetzable@gmail.com>
2018-07-27 14:53:47 -0500

    Subiendo cambios de estilo, personalizado

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,7 @@ window.newAccount = (email, password) => {
       verifyAccountWithEmail();
       alert('Se ha enviado un correo a tu email para verificar tu cuenta.');
       signOutUser();
+      location.href = ('../index.html');
     })
     .catch((error) => {
       // Handle Errors here.
@@ -60,6 +61,8 @@ window.loginUser = (email, password) => {
         alert('Por favor, verifica tu contraseña.');
       } else if (errorCode === 'auth/user-not-found' || errorCode === 'auth/invalid-email' || errorCode === 'auth/argument-error') {
         alert('Por favor verifica tu usuario o Registrate para poder iniciar sesión.');
+      } else if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('El correo ya ha sido registrado');
       }
     });
 };
@@ -86,7 +89,9 @@ window.googleUserLogin = () => {
       let email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       let credential = error.credential;
-      console.log(errorCode);
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('El correo ya ha sido registrado');
+      }
       // ...
     });
 };
@@ -111,7 +116,9 @@ window.facebookUserLogin = () => {
       let email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       let credential = error.credential;
-      console.log(errorCode);
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('El correo ya ha sido registrado');
+      }
       // ...
     });
 };
@@ -135,7 +142,9 @@ window.twitterUserLogin = () => {
       let email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       let credential = error.credential;
-      console.log(errorCode);
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('El correo ya ha sido registrado');
+      }
       // ...
     });
 };
@@ -159,7 +168,9 @@ window.githubUserLogin = () => {
       let email = error.email;
       // The firebase.auth.AuthCredential type that was used.
       let credential = error.credential;
-      console.log(errorCode);
+      if (credential === 'auth/account-exists-with-different-credential') {
+        alert('El correo ya ha sido registrado');
+      }
       // ...
     });
 };
@@ -193,7 +204,7 @@ window.addingDataToNewsfeed = (input) => {
       let email = user.email;
       let photoURL = user.photoURL;
       let uid = user.uid;
-      let postTime = `${new Date()}`;
+      let postTime = firebase.firestore.FieldValue.serverTimestamp();
       db.collection('posts').add({
         username: displayName,
         postInput: input,
@@ -282,3 +293,65 @@ window.likePost = (postId, userAddingLike) => {
       console.log(error);
     });
 };
+
+window.passwordReset = (userEmail) => {
+  let auth = firebase.auth();
+  let emailAddress = userEmail;
+
+  auth.sendPasswordResetEmail(emailAddress)
+    .then(() => {
+      // Email sent.
+      alert('Se ha enviado un mail a tu correo para poder recuperar tu contraseña.');
+      location.href = ('../index.html');
+    }).catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+};
+
+window.createUserProfileWithEmail = (name, email, location) => {
+  db.collection('users').add({
+    userName: name,
+    userEmail: email,
+    city: location
+  })
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+};
+
+window.editPost = (postId, postUserID) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user.email === postUserID) {
+      createUpdateArea(postId);
+    } else {
+      alert('No puedes modificar publicaciones de otros garnacheros');
+    }
+  });
+};
+
+window.savePostEdition = (postId) => {
+  let newPostInput = document.getElementById(`post${postId}`).value;
+  db.collection('posts').doc(postId).get()
+    .then(post => {
+      db.collection('posts').doc(postId).update({
+        postInput: newPostInput
+      })
+        .then(result => {
+          printUserPost();
+        })
+        .catch(error => {
+          console.log(error);
+          alert('No pudimos editar tu publicación, intentalo de nuevo.');
+        });
+    });
+};
+
+// window.updateInRealTime = () => {
+//   let printedPosts = document.getElementById('list-post');
+//   const dbRef = firebase.database().ref().child('printedPosts');
+//   dbRef.onSnapshot(snap => printedPosts.innerHTML = snap.val());
+// };  
